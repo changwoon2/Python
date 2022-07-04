8a28ccb56a14dc55176ac34f9ea31c6a71dbecab
changwoon2 <changwoon4@naver.com>
2022-07-04 22:37:22 +0900

    save

---
+++ gold.py
@@ -0,0 +1,27 @@
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+3)
+print(2*8)
+print(3*(3+1))
+# 참 / 거짓 
+print(5 > 10)
+print(5 < 10)
+print(not True)
+print(not False)
+
+# 애완동물 소개해주세요 변수
+name ="강아지"
+animal ="연탄이"
+age = 4
+hobby ="산책"
+is_adult = age >= 3
+
+print("우리집 " + animal + " 이름은 " + name + "예요")
+print(name + "는" + str(age) + " 살이며, "+ hobby + "을 아주 좋아해요")
+print(name + "는 어른일까요? " + str (is_adult))
+
+from random import *
+
+print(random())
