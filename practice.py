bd7a20f86c0089cdde84d59b05e8ee70ead0bd12
changwoon2 <changwoon4@naver.com>
2022-12-18 01:03:50 +0900

    자료형

---
+++ practice.py
@@ -0,0 +1,34 @@
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+3)
+print(2*8)
+print(3*(3+1))
+print('풍선')
+print("나비")
+print("ㅋ"*9)
+
+
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(False)
+print(not True)
+print(not False)
+print(not (5 > 10))
+
+# 애완동물 / 소개
+animal = "고양이"
+name = "해피"
+age = 4
+hobby = "낮잠"
+is_adult = age >= 3
+
+print('우리집 ' + animal + "의 이름은 " + name + "예요")
+hobby = "공놀이"
+print(name, "는 " , age, "살이며," , hobby, "을 좋아해요")
+print(name + "는 어른인가요" + str(is_adult))
+
+station = "사당" 
+print(station + "행 열차가 들어오고 있습니다.")
